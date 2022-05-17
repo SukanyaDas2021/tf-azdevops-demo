887754cf2a7368f70c8bf9c177d6e3df5b2b2b3a
Sukanya <90558127+SukanyaDas2021@users.noreply.github.com>
2022-05-17 21:37:56 +0530

    Add files via upload

---
+++ main.tf
@@ -0,0 +1,28 @@
+terraform {
+
+  required_version = ">=0.12"
+
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~>2.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "rg" {
+  name     = "store-rg"
+  location = "eastus"
+}
+
+resource "azurerm_storage_account" "test" {
+  name                     = "sukanyastorageaccount"
+  resource_group_name      = azurerm_resource_group.rg.name
+  location                 = azurerm_resource_group.rg.location
+  account_tier             = "Standard"
+  account_replication_type = "GRS"
+}
